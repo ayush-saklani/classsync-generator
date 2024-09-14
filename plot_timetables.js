e70cc07132a7fcdceeb7ff05a421d1504672b353
“ayush <ayushsaklani04@gmail.com>
2024-09-14 22:53:13 +0530

    half mornign and half evening shift

--- plot_timetables.js
+++ plot_timetables.js
@@ -42,30 +42,46 @@ const validate_timetable_slot = (alltimetable, j, k, teacherid, classid) => {
 }
 
 let flag = 0;   // flag to check the number of conflicts in the timetable generation
-let number_of_sections = 5;     // number of timetable to generate
+let number_of_sections = alltimetable.length;
+let morning_batch = Math.ceil(number_of_sections / 2);     // number of timetable to generate 
 for (let i = 0; i < alltimetable.length; i++) {
     console.log("Generating Timetables for Section " + i);
-    let subjects = JSON.parse(JSON.stringify(alltimetable[i].subjects));        // deep copy of subjects
-    let timetable = JSON.parse(JSON.stringify(timetablestructure));             // deep copy of timetablestructure
-    while (subjects.length > 0) {                                               // loop until all subjects are assigned
-        let temp_subject_index = (Math.floor(Math.random() * subjects.length));   // randomly choose subject\
-        let temp_room_index = (Math.floor(Math.random() * room.length));                  // randomly choose room
+    let subjects = JSON.parse(JSON.stringify(alltimetable[i].subjects));            // deep copy of subjects
+    let timetable = JSON.parse(JSON.stringify(timetablestructure));                 // deep copy of timetablestructure
+    while (subjects.length > 0) {                                                   // loop until all subjects are assigned
+        let temp_subject_index = (Math.floor(Math.random() * subjects.length));     // randomly choose subject
+        let temp_room_index = (Math.floor(Math.random() * room.length));            // randomly choose room
 
         if (subjects[temp_subject_index] && room[temp_room_index]) {
-            let temp = Math.floor(Math.random() * max);                         // randomly choose slot
+
+            // =================================  Randomly assign subject to slot  =================================
+            // ============== this below logic will make half timetable morning and half evening shift =============
+            let temp;
+            while (1) {
+                temp = Math.floor(Math.random() * max);                         // randomly choose slot
+                if (morning_batch == 0 && (Math.floor(temp % 10) >= 5)) {       // if morning batch is over then choose only afternoon slots
+                    break;
+                }
+                else if (morning_batch > 0 && (Math.floor(temp % 10) < 5)) {   // if morning batch is not over then choose only morning slots
+                    break;
+                }
+            }
+            // ============== this above logic will make half timetable morning and half evening shift =============
+            // let temp = Math.floor(Math.random() * max);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      // old logic to assign subject to slot (randomly)
+
 
             // console.log(temp + " || " + (Math.floor(temp / 10)) + " || " + (Math.floor((temp % 10))));
             if (timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].teacherid == "" && timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].classid == "") {
 
                 //if validation in slot is true then assign the subject to that slot
-                if (validate_timetable_slot(alltimetable, (Math.floor(temp / 10)), (Math.floor(temp % 10)), subjects[temp_subject_index].teacherid, room[temp_room_index].roomid)) {
+                if (validate_timetable_slot(alltimetable, (Math.floor(temp / 10)), (Math.floor(temp % 10)), subjects[temp_subject_index].teacherid, room[temp_room_index].roomid, morning_batch)) {
                     timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].teacherid = subjects[temp_subject_index].teacherid;
                     timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].classid = room[temp_room_index].roomid;
                     subjects[temp_subject_index].weekly_hrs--;
                     // room[temp_room_index].capacity--;                        // currently not implemented due to logic thinking incapability of the developer, lol :P
                     if (subjects[temp_subject_index].weekly_hrs == 0) {
                         subjects.splice(temp_subject_index, 1);                 // remove subject from list and 
-                    }if (room[temp_room_index].capacity == 0) {
+                    } if (room[temp_room_index].capacity == 0) {
                         room.splice(temp_room_index, 1);                        // remove room from list (currently not implemented) due to above reason
                     }
                 } else {
@@ -76,10 +92,11 @@ for (let i = 0; i < alltimetable.length; i++) {
         }
     }
     console.log("===========================================================");
+    morning_batch = (morning_batch > 0) ? (morning_batch - 1) : morning_batch;
     alltimetable[i].timetable = timetable;
 }
 console.log("=============  Timetable Generation Complete  =============");
-console.log("==========  Timetable validation status : "+validate_timetable_set(alltimetable)+"    =========");
+console.log("==========  Timetable validation status : " + validate_timetable_set(alltimetable) + "    =========");
 console.log("===========================================================");
 fs.writeFileSync('data2.json', JSON.stringify(alltimetable), 'utf8');
 
