29d0eb1f655cde5523a176add6ff3397f3e3ff2e
“ayush <ayushsaklani04@gmail.com>
2024-09-15 00:38:22 +0530

    Refactor timetable generation logic and add validation functions for teacher multiple slot in a day

--- plot_timetables.js
+++ plot_timetables.js
@@ -13,13 +13,29 @@ let timetablestructure = [
     [{ "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }, { "classid": "", "teacherid": "" }]
 ];
 
-//  room data
+// Timtable in 2D-array (just for reference)
+//  0  1  2  3  4  5  6  7  8  9        monday
+// 10 11 12 13 14 15 16 17 18 19        tuesday
+// 20 21 22 23 24 25 26 27 28 29        wednesday
+// 30 31 32 33 34 35 36 37 38 39        thursday
+// 40 41 42 43 44 45 46 47 48 49        friday
+// 50 51 52 53 54 55 56 57 58 59        saturday
+// 60 61 62 63 64 65 66 67 68 69        sunday
+//              |
+//             \/
+//           index = [6,4] in 2D array   and yes i made it by myself not AI :P
+
+
+//  slot range for timetable
+let min = 10; // start from 10 (tuesday 9am)   
+let max = 60; // end at 59  (saturday 6pm) (60 is not included)
+
+//  timetable data with subjects and teachers already assigned
+let alltimetable = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+//  room data with room id and capacity (capacity is not implemented in this code right now)
 let room = JSON.parse(fs.readFileSync('room.json', 'utf8'));
 
-let max = 60; // 6 days * 10 hours mon to sat 8am to 6pm
-
-let alltimetable = JSON.parse(fs.readFileSync('data.json', 'utf8'));
-
+//  this function will validate the slot of timetable and flag out the room and teacher conflicts from that batch of timetables
 const validate_timetable_slot = (alltimetable, j, k, teacherid, classid) => {
     // 8am to 6pm
     let temp = {}
@@ -40,7 +56,19 @@ const validate_timetable_slot = (alltimetable, j, k, teacherid, classid) => {
         return true;
     }
 }
-
+// this function will prevent the teacher to teach in multiple slots in a day
+const validate_multiple_slot_in_a_day = (timetable, j, k, teacherid, classid) => {    // this function will prevent the teacher to teach in multiple slots in a day
+    let map = {}
+    for (let i = 0; i < 10; i++) {
+        map[timetable[j][i].teacherid] = true;
+    }
+    if (map[teacherid]) {
+        console.log("false");
+        return false;
+    } else {
+        return true;
+    }
+}
 let flag = 0;   // flag to check the number of conflicts in the timetable generation
 let number_of_sections = alltimetable.length;
 let morning_batch = Math.ceil(number_of_sections / 2);     // number of timetable to generate 
@@ -54,19 +82,20 @@ for (let i = 0; i < alltimetable.length; i++) {
 
         if (subjects[temp_subject_index] && room[temp_room_index]) {
 
-            // =================================  Randomly assign subject to slot  =================================
-            // ============== this below logic will make half timetable morning and half evening shift =============
+            // =====================================  Randomly assign subject to slot  =======================================
+            // =========== this above logic will make half timetable of that batch morning and half evening shift ============
             let temp;
             while (1) {
-                temp = Math.floor(Math.random() * max);                         // randomly choose slot
+                temp = Math.floor(Math.random() * (max - min)) + min;           // randomly choose slot btwn 10 to 59 (just for testing)
+                // temp = Math.floor(Math.random() * max);                      // randomly choose slot
                 if (morning_batch == 0 && (Math.floor(temp % 10) >= 5)) {       // if morning batch is over then choose only afternoon slots
                     break;
                 }
-                else if (morning_batch > 0 && (Math.floor(temp % 10) < 5)) {   // if morning batch is not over then choose only morning slots
+                else if (morning_batch > 0 && (Math.floor(temp % 10) < 5)) {    // if morning batch is not over then choose only morning slots
                     break;
                 }
             }
-            // ============== this above logic will make half timetable morning and half evening shift =============
+            // =========== this above logic will make half timetable of that batch morning and half evening shift ============
             // let temp = Math.floor(Math.random() * max);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      // old logic to assign subject to slot (randomly)
 
 
@@ -75,14 +104,18 @@ for (let i = 0; i < alltimetable.length; i++) {
 
                 //if validation in slot is true then assign the subject to that slot
                 if (validate_timetable_slot(alltimetable, (Math.floor(temp / 10)), (Math.floor(temp % 10)), subjects[temp_subject_index].teacherid, room[temp_room_index].roomid, morning_batch)) {
-                    timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].teacherid = subjects[temp_subject_index].teacherid;
-                    timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].classid = room[temp_room_index].roomid;
-                    subjects[temp_subject_index].weekly_hrs--;
-                    // room[temp_room_index].capacity--;                        // currently not implemented due to logic thinking incapability of the developer, lol :P
-                    if (subjects[temp_subject_index].weekly_hrs == 0) {
-                        subjects.splice(temp_subject_index, 1);                 // remove subject from list and 
-                    } if (room[temp_room_index].capacity == 0) {
-                        room.splice(temp_room_index, 1);                        // remove room from list (currently not implemented) due to above reason
+                    
+                    // if validation in a day is true then assign the subject to that slot (this will prevent the teacher to teach in multiple slots in a day)
+                    if (validate_multiple_slot_in_a_day(timetable, (Math.floor(temp / 10)), (Math.floor(temp % 10)), subjects[temp_subject_index].teacherid, room[temp_room_index].roomid)) {
+                        timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].teacherid = subjects[temp_subject_index].teacherid;
+                        timetable[(Math.floor(temp / 10))][(Math.floor(temp % 10))].classid = room[temp_room_index].roomid;
+                        subjects[temp_subject_index].weekly_hrs--;
+                        // room[temp_room_index].capacity--;                        // currently not implemented due to logic thinking incapability of the developer, lol :P
+                        if (subjects[temp_subject_index].weekly_hrs == 0) {
+                            subjects.splice(temp_subject_index, 1);                 // remove subject from list and 
+                        } if (room[temp_room_index].capacity == 0) {
+                            room.splice(temp_room_index, 1);                        // remove room from list (currently not implemented) due to above reason
+                        }
                     }
                 } else {
                     flag++;
@@ -105,12 +138,13 @@ fs.writeFileSync('data2.json', JSON.stringify(alltimetable), 'utf8');
 
 
 // Timtable in 2D-array (just for reference)
-//  0  1  2  3  4  5  6  7  8  9
-// 10 11 12 13 14 15 16 17 18 19
-// 20 21 22 23 24 25 26 27 28 29
-// 30 31 32 33 34 35 36 37 38 39
-// 40 41 42 43 44 45 46 47 48 49
-// 50 51 52 53 54 55 56 57 58 59
+//  0  1  2  3  4  5  6  7  8  9        monday
+// 10 11 12 13 14 15 16 17 18 19        tuesday
+// 20 21 22 23 24 25 26 27 28 29        wednesday
+// 30 31 32 33 34 35 36 37 38 39        thursday
+// 40 41 42 43 44 45 46 47 48 49        friday
+// 50 51 52 53 54 55 56 57 58 59        saturday
+// 60 61 62 63 64 65 66 67 68 69        sunday
 //              |
 //             \/
-//           index = [5,4] in 2D array
+//           index = [6,4] in 2D array   and yes i made it by myself not AI :P and yes it is copied from above :P, 
