5e3cbf0a06a6de433a5a51aecd18db866cfdd627
“ayush <ayushsaklani04@gmail.com>
2024-10-02 01:16:44 +0530

    updates

--- test.js
+++ test.js
@@ -1,18 +1,19 @@
 import fs from 'fs';
 import initialize_population from "./plot_timetables.js";
+import tournamentSelection from './selection.js';
 
 const initialize_population_population = (showstats = false) => {
     let min = 0;
     let max = 50;
     let population = []
     let counter = 0;
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < 100; i++) {
         counter += 1;
         console.log("counter: ", counter + " i: ", i);
         let alltimetable = JSON.parse(fs.readFileSync('data.json', 'utf8'));    //  timetable data with subjects and teachers already assigned
         let room = JSON.parse(fs.readFileSync('room.json', 'utf8'));            //  (capacity is not implemented in this code right now)
         let timetable = initialize_population(alltimetable, room, min, max, showstats);
-        if(timetable.fitness < -1000){
+        if(timetable.fitness < 150){
             i--;
             continue;
         }else{
@@ -23,6 +24,6 @@ const initialize_population_population = (showstats = false) => {
     return population;
 }
 let population = initialize_population_population(false);
+population = tournamentSelection(population, 3, 0.3);
 console.log(population.length);
-population.sort((a, b) => b.fitness - a.fitness);
-fs.writeFileSync('population.json', JSON.stringify(population, null, 4), 'utf8');
+fs.writeFileSync('population_selected.json', JSON.stringify(population, null, 4), 'utf8');
