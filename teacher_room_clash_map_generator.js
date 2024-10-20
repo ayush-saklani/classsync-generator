a7ddbdd6b887987f11788218078f8aaf5b67a407
“ayush <ayushsaklani04@gmail.com>
2024-10-21 00:08:29 +0530

    fucntion to shorten the logic

---
+++ teacher_room_clash_map_generator.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+
+const teacher_room_clash_map_generator = (population) => {
+    for (let x = 0; x < population.length; x++) {
+        let teacher_room_clash_map = {};
+        for (let i = 0; i < population[x]['data'].length; i++) {
+            for (let j = 0; j < 7; j++) {
+                for (let k = 0; k < 10; k++) {
+                    if (population[x]['data'][i]['timetable'][j][k].teacherid == "" && population[x]['data'][i]['timetable'][j][k].roomid == "") continue;
+                    let room_checker = "room" + ";" + j + ";" + k + ";" + population[x]['data'][i]['timetable'][j][k].roomid;
+                    let teacher_checker = "teacher" + ";" + j + ";" + k + ";" + population[x]['data'][i]['timetable'][j][k].teacherid;
+                    // if(teacher_room_clash_map[room_checker] || teacher_room_clash_map[teacher_checker]) {
+                        // console.log("Conflict found at " + i + " " + j + " " + k);
+                    // }
+                    teacher_room_clash_map[room_checker] = true;
+                    teacher_room_clash_map[teacher_checker] = true;
+                }
+            }
+        }
+        process.stdout.write(Object.keys(teacher_room_clash_map).length + " ");
+        population[x]['teacher_room_clash_map'] = teacher_room_clash_map;
+    }
+    // console.log();
+    return population;
+}
+
+export default teacher_room_clash_map_generator;
+
+// example usage:
+// let population = JSON.parse(fs.readFileSync('population_selected.json', 'utf8'));
+// population = teacher_room_clash_map_generator(population);
+// fs.writeFileSync('./JSONdata/teacher_room_clash_map.json', JSON.stringify(population, null, 4), 'utf8');
