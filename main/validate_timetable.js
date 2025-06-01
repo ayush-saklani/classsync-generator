cf9add7a732b8b46eb5a5a0f254bc6f4129b5413
ayush saklani <ayushsaklani04@gmail.com>
2025-06-01 22:56:41 +0530

    refactor and renamed

--- main/validate_timetable.js
+++ main/validate_timetable.js
@@ -2,106 +2,26 @@
 import fs from "fs";
 const validate_timetable = (timetable) => {
   let day = ["Mon", "Tue", "Wedn", "Thu", "Fri", "Sat", "Sun"];
-  let slot = [
-    "08-09",
-    "09-10",
-    "10-11",
-    "11-12",
-    "12-01",
-    "01-02",
-    "02-03",
-    "03-04",
-    "04-05",
-    "05-06",
-  ];
-  for (let j = 0; j < 7; j++) {
-    // monday to sunday
-    for (let k = 0; k < 10; k++) {
-      // 8am to 6pm
+  let slot = ["08-09", "09-10", "10-11", "11-12", "12-01", "01-02", "02-03", "03-04", "04-05", "05-06",];
+  for (let j = 0; j < 7; j++) {                                 // monday to sunday
+    for (let k = 0; k < 10; k++) {                              // 8am to 6pm
       let temp = {};
-      for (let i = 0; i < timetable["data"].length; i++) {
-        // all timetables
-        if (
-          timetable["data"][i]["timetable"][j][k].roomid != "" ||
-          timetable["data"][i]["timetable"][j][k].teacherid != ""
-        ) {
+      for (let i = 0; i < timetable["data"].length; i++) {      // all timetables
+        if (timetable["data"][i]["timetable"][j][k].roomid != "" || timetable["data"][i]["timetable"][j][k].teacherid != "") {
           continue;
-        } else if (
-          timetable["data"][i]["timetable"][j][k].teacherid &&
-          timetable["data"][i]["timetable"][j][k].roomid
-        ) {
+        } else if (timetable["data"][i]["timetable"][j][k].teacherid && timetable["data"][i]["timetable"][j][k].roomid) {
           // if () {         // if room is empty
-          if (
-            temp[
-              "teacher" +
-                ";" +
-                j +
-                ";" +
-                k +
-                ";" +
-                timetable["data"][i]["timetable"][j][k].teacherid
-            ] ||
-            temp[
-              "room" +
-                ";" +
-                j +
-                ";" +
-                k +
-                ";" +
-                timetable["data"][i]["timetable"][j][k].roomid
-            ]
-          ) {
-            process.stdout.write(
-              temp["room" + timetable["data"][i]["timetable"][j][k].roomid]
-                ? "room conflicts "
-                : "room error safe",
-            );
+          if (temp["teacher" + ";" + j + ";" + k + ";" + timetable["data"][i]["timetable"][j][k].teacherid] || temp["room" + ";" + j + ";" + k + ";" + timetable["data"][i]["timetable"][j][k].roomid]) {
+            process.stdout.write(temp["room" + timetable["data"][i]["timetable"][j][k].roomid] ? "room conflicts " : "room error safe",);
             process.stdout.write("    --------    ");
-            process.stdout.write(
-              temp[
-                "teacher" + timetable["data"][i]["timetable"][j][k].teacherid
-              ]
-                ? "teacher conflicts "
-                : "teacher error safe",
-            );
+            process.stdout.write(temp["teacher" + timetable["data"][i]["timetable"][j][k].teacherid] ? "teacher conflicts " : "teacher error safe",);
             process.stdout.write("    --------    ");
-            process.stdout.write(
-              i +
-                " " +
-                j +
-                " " +
-                k +
-                " conflict at" +
-                " Day : " +
-                day[j] +
-                " Period : " +
-                slot[j] +
-                " room id : " +
-                timetable["data"][i]["timetable"][j][k].roomid +
-                " Teacher id : " +
-                timetable["data"][i]["timetable"][j][k].teacherid,
-            );
+            process.stdout.write(i + " " + j + " " + k + " conflict at" + " Day : " + day[j] + " Period : " + slot[j] + " room id : " + timetable["data"][i]["timetable"][j][k].roomid + " Teacher id : " + timetable["data"][i]["timetable"][j][k].teacherid,);
             console.log("");
             return false;
           } else {
-            temp[
-              "room" +
-                ";" +
-                j +
-                ";" +
-                k +
-                ";" +
-                timetable["data"][i]["timetable"][j][k].roomid
-            ] = true;
-            temp[
-              "teacher" +
-                ";" +
-                j +
-                ";" +
-                k +
-                ";" +
-                timetable["data"][i]["timetable"][j][k].teacherid
-            ] = true;
+            temp["room" + ";" + j + ";" + k + ";" + timetable["data"][i]["timetable"][j][k].roomid] = true;
+            temp["teacher" + ";" + j + ";" + k + ";" + timetable["data"][i]["timetable"][j][k].teacherid] = true;
           }
         }
       }
